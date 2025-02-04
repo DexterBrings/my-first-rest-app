b8d37ff61fa39c50ab5b643f659cb36252d924f6
Dexter Brings <dexter.brings@gmail.com>
2025-02-04 18:25:31 +0100

    Request ergänst.

--- src/main/java/com/example/my_first_rest_app/ToDoController.java
+++ src/main/java/com/example/my_first_rest_app/ToDoController.java
@@ -1,9 +1,12 @@
 package com.example.my_first_rest_app;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 public class ToDoController {
@@ -14,12 +17,19 @@ public class ToDoController {
     @GetMapping ("/todo")
     public ResponseEntity<Todo> get(@RequestParam(value = "id") int id){
 
-        Todo newTodo = new Todo();
-        newTodo.setId(id);
-        newTodo.setDescription("Einkaufen gehen");
-        newTodo.setIsDone(true);
+        // get todo from db by id
+        Optional<Todo> todoInDb = todoRepository.findById(id);
 
-        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+        if(todoInDb.isPresent()){
+            return new ResponseEntity<Todo>(todoInDb.get(), HttpStatus.OK);
+        }
+        return new ResponseEntity("No todo found with id " + id, HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping ("/todo/all")
+    public ResponseEntity<Iterable<Todo>> getAll(){
+        Iterable<Todo> allTodosInDb = todoRepository.findAll();
+        return new ResponseEntity<Iterable<Todo>>(allTodosInDb, HttpStatus.OK);
     }
 
     @PostMapping ("/todo")
@@ -27,5 +37,18 @@ public class ToDoController {
         //save todo in db
         todoRepository.save(newTodo);
         return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+    }
+
+    @DeleteMapping ("/todo")
+    public ResponseEntity delete(@RequestParam(value = "id") int id){
+
+        Optional<Todo> todoInDb = todoRepository.findById(id);
+
+        if(todoInDb.isPresent()){
+            todoRepository.deleteById(id);
+            return new ResponseEntity("Todo deleted", HttpStatus.OK);
+        }
+
+        return new ResponseEntity("No todo to delete found with id " + id, HttpStatus.NOT_FOUND);
     }
 }
