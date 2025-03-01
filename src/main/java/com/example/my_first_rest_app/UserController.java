8fa1b9817da23c017d68838ffa13ff15afbfa7d6
Dexter Brings <dexter.brings@gmail.com>
2025-03-01 17:39:53 +0100

    User und UserController angepasst.

--- src/main/java/com/example/my_first_rest_app/UserController.java
+++ src/main/java/com/example/my_first_rest_app/UserController.java
@@ -1,4 +1,22 @@
 package com.example.my_first_rest_app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody User newUser){
+        var savedUser = userRepository.save(newUser);
+        return new ResponseEntity<User>(savedUser, HttpStatus.CREATED);
+    }
 }
